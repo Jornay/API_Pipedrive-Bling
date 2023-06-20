409f7292939f32e899e3f1e44f93b0e1256abf57
Jornay <lucasdeafg@yahoo.com.br>
2023-06-20 16:40:42 -0300

    fix.pipedrive-access

--- src/services/apiPipedrive.ts
+++ src/services/apiPipedrive.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { pipedrive_api_url } from '../config/pipedriveUrlConnection'
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 const api = axios.create({
     baseURL: pipedrive_api_url,
     headers: { "Content-Type": "application/json" },
     params: {
         "status": "won",
-        "api_token": "65c70bb47ceb62783123c8420c13e78cc18fb5f8"
+        "api_token": process.env.PIPEDRIVE_API_TOKEN
     }
 })
 console.log({ "baseURL": pipedrive_api_url })
