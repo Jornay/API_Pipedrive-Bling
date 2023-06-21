0f7c583ea01f37bded9847deaf50927bf3e76d91
Jornay <lucasdeafg@yahoo.com.br>
2023-06-21 18:05:14 -0300

    fix.router.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,29 +1,53 @@
+import express from "express";
 import { Router } from "express";
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DealsWithWonController } from "./controllers/DealsWithWonController";
 
-dotenv.config()
+dotenv.config();
 
-const mongoUriConnection = process.env.DB_URI
-
-if (!mongoUriConnection) {
-    throw new Error('MongoDB connection URI is missing');
-}
-
-//Connectando no mongo
-mongoose.connect(mongoUriConnection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
-//----
-
+const app = express();
 const router = Router();
 
 const dealsWithWonController = new DealsWithWonController();
 
-router.post("/users", dealsWithWonController.createDeals);
-router.get("/users", dealsWithWonController.getAllDeals)
+app.use(express.json());
 
-export { router };
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Configura o cabeçalho Access-Control-Allow-Origin para permitir todas as origens
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Configura os métodos HTTP permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Configura os cabeçalhos permitidos
+    next();
+  });
+
+const mongoUriConnection = process.env.DB_URI;
+
+if (!mongoUriConnection) {
+  throw new Error('MongoDB connection URI is missing');
+}
+
+// Conectando no mongo
+mongoose.connect(mongoUriConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
+  process.exit(1); // Termina o processo em caso de erro de conexão
+});
+
+// Rotas
+router.post("/users", dealsWithWonController.createDeals);
+router.get("/users", dealsWithWonController.getAllDeals);
+
+app.use(router);
+
+// Iniciar o servidor
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+export {router}
